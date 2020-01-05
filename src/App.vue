f870eb8285759a54a1d6c70e17d8552e59905a52
Andre <andregidoin@gmail.com>
2020-01-05 23:43:08 +0100

    updated frontpage design

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,65 @@
           <router-view />
       </div>
 
-  <div class="infoContainer nr1">
-
-        <div class="infoItems color"><span class="imgItem"><img src="../public/graphics/WorldMap_CRC_White-Black.gif"></span></div>
-        <div class="infoItems"><span class="item"><h1>30 years ago, the United Nations created the Convention on the Right of the Child (CRC). Every UN country has ratified it and are bound to it by international law. <br><br><b>Every country except the USA.</b></h1></span></div>
+  <!-- FIRST SECTION - RED -->
+  <div class="videoContainer">
+    <div class="videoInfo red">
+      <video autoplay muted loop class="thevideos">
+        <source src="./assets/video/Red.mp4" type="video/mp4">
+      </video>
+      </div>
   </div>
-  <div class="infoContainer nr2">
-
-        <div class="infoItems color"><span class="imgItem"><img src="../public/graphics/Articles_CRC-2_2.gif"></span></div>
-        <div class="infoItems"><span class="item"><h1>Adopt the Rights of a Child is a project about democratizing the ratification of children’s rights, allowing people to support the convention regardless of what their government might say. So choose an article you want to support and ratify it yourself by adopting and donating money to that right. </h1></span></div>
+  <div class="textContainer mediumHeight">
+        <div class="textInfo red ">
+          <div class="whiteTextBlock mediumHeight">
+          <h1>30 years ago, the United Nations created the Convention on the Right of the Child (CRC). Every UN country has ratified it and are bound to it by international law. <br><br><b>Every country except the USA.</b></h1>
+          </div>
+        </div>
+      <div class="halfColor blue">
+      </div>
   </div>
-  <div class="infoContainer nr3">      
-        <div class="infoItems color"><span class="imgItem"><img src="../public/graphics/42_Articles_Animation-2.gif"></span></div>
-        <div class="infoItems"><span class="item"><h1>The CRC is a human rights treaty based on 42 articles which sets out the civil, political, economic, social, health and cultural rights of children. It goes without saying that governments should support it and create laws meant to protect children. This project aims to let people take the power into our own hands and make sure that all our children have a safe and nourishing place to live and grow.</h1></span></div>
-            
+
+  <!-- SECOND SECTION - BLUE -->
+    <div class="videoContainer">
+    <div class="videoInfo blue">
+      <video autoplay muted loop class="thevideos">
+        <source src="./assets/video/Blue.mp4" type="video/mp4">
+      </video>
+    </div>
+  </div>
+  <div class="textContainer largeHeight">
+        <div class="textInfo blue">
+          <div class="whiteTextBlock largeHeight">
+            <h1>
+            Adopt the Rights of a Child is a project about democratizing the ratification of children’s rights, allowing people to support the convention regardless of what their government might say. So choose an article you want to support and ratify it yourself by adopting and donating money to that right.
+          </h1>
+          </div>
+        </div>
+      <div class="halfColor yellow">
+      </div>
   </div>
 
-  <div class="footer">
+  <!-- THIRD SECTION - YELLOW -->
+  <div class="videoContainer">
+    <div class="videoInfo yellow">
+      <video autoplay muted loop class="thevideos">
+        <source src="./assets/video/Yellow.mp4" type="video/mp4">
+      </video>
+    </div>
+  </div>
+  <div class="textContainer largeHeight">
+        <div class="textInfo yellow">
+          <div class="whiteTextBlock largeHeight">
+          <h1>
+            The CRC is a human rights treaty based on 42 articles which sets out the civil, political, economic, social, health and cultural rights of children. It goes without saying that governments should support it and create laws meant to protect children. This project aims to let people take the power into our own hands and make sure that all our children have a safe and nourishing place to live and grow.
+            </h1>
+          </div>
+        </div>
+      <div class="halfColor">
+      </div>
+  </div>
+
+  <div class="footer yellow">
     <div class="social"><a href="https://www.instagram.com/adoptthecrcproject/" target="_blank"><img src="../public/graphics/instagram2.png"><br><p>Follow the project on Instagram</p></a></div>
     <div class="footerContent">This is a fundraiser for UNICEF. All proceeds are directly donated to them and their work to protect children's rights. This project was created pro-bono by <a href="http://blockchainclub.io" target="_blank">The Experimental Blockchain Club</a>. For any questions please send an email to <a href="mailto:contact@blockchainclub.io">contact@blockchainclub.io</a>
     </div>
@@ -171,7 +213,6 @@ b {
     margin: auto;
     font-size: 0.6em;
     line-height: 2.7em;
-    width:65%;
     height: 350px;
     box-sizing: border-box;
     padding: 10px;
@@ -275,5 +316,164 @@ b {
 }
 
 
+
+// NEW STYLES
+
+.videoContainer {
+  width: 100vw;
+  height: 60vh;
+  margin: auto;
+  padding: 0; 
+  @media only screen and (min-width: 780px) {
+      margin-left: 35px;
+      }
+}
+.videoInfo {
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  margin: auto;
+  padding: 0;
+  img {
+    height: 100%;
+  }
+}
+
+.textContainer {
+  width: 100vw;
+  position: relative;
+    @media only screen and (min-width: 780px) {
+      margin-left: 35px;
+      }
+}
+
+.textInfo {
+  margin: auto;
+  padding: 0;
+}
+.whiteTextBlock {
+  margin: auto;
+  padding: 0 40px;
+  box-sizing: border-box;
+  position: relative;
+  z-index: 1;
+  width: 80vw;
+  background-color: white;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  filter: drop-shadow(14px 14px 10px rgba(0, 0, 0, 0.04));
+    h1 {
+      vertical-align: middle;
+      align-self: center;
+      line-height: 1.4em;
+      font-size: 1.8em;
+    }
+}
+
+.halfColor { 
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50%;
+}
+.red {
+    background-color: #d74246;
+  }
+.yellow {
+  background-color: #fecb3e;
+}
+.blue {
+  background-color: #4daed1;
+}
+.thevideos {
+  height: 100%;
+}
+
+.largeHeight {
+  height: 250px;
+}
+.mediumHeight {
+  height: 200px;
+}
+.smallHeight {
+  height: 150px;
+}
+
+
+// SCALABLE
+
+@media only screen and (max-width: 780px) {
+  .whiteTextBlock {
+    clip-path: polygon(0% 4%, 2% 0%, 98% 0%, 100% 4%, 100% 96%, 98% 100%, 2% 100%, 0 96%);
+  }
+  .whiteTextBlock h1 {
+  font-size: 1em;
+  }
+  .thevideos {
+  width: 100%;
+  height: auto;
+}
+.videoContainer {
+  height: 45vh;
+}
+  .footer {
+    padding-bottom: 40px;
+  }
+}
+
+@media only screen and (min-width: 780px) {
+  .whiteTextBlock {
+    clip-path: polygon(0% 4%, 2% 0%, 98% 0%, 100% 4%, 100% 96%, 98% 100%, 2% 100%, 0 96%);
+  }
+  .whiteTextBlock h1 {
+  font-size: 1.3em;
+  }
+  .largeHeight {
+  height: 300px;
+}
+.mediumHeight {
+  height: 250px;
+}
+.smallHeight {
+  height: 250px;
+}
+}
+
+@media only screen and (min-width: 985px) {
+  .whiteTextBlock {
+    clip-path: polygon(0% 6%, 2% 0%, 98% 0%, 100% 6%, 100% 94%, 98% 100%, 2% 100%, 0 94%);
+  }
+  .whiteTextBlock h1 {
+  font-size: 1.4em;
+  }
+  .largeHeight {
+  height: 340px;
+}
+.mediumHeight {
+  height: 300px;
+}
+.smallHeight {
+  height: 250px;
+}
+}
+@media only screen and (min-width: 1000px) {
+  .whiteTextBlock {
+    clip-path: polygon(0% 7%, 2% 0%, 98% 0%, 100% 7%, 100% 93%, 98% 100%, 2% 100%, 0 93%);
+  }
+  .whiteTextBlock h1 {
+  font-size: 1.5em;
+  }
+}
+@media only screen and (min-width: 1101px) {
+  .whiteTextBlock {
+    clip-path: polygon(0% 7%, 2% 0%, 98% 0%, 100% 7%, 100% 93%, 98% 100%, 2% 100%, 0 93%);
+  }
+  .whiteTextBlock h1 {
+  font-size: 1.7em;
+  }
+}
 
 </style>
